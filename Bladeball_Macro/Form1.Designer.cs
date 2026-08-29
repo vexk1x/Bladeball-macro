a6d0a36c28a59ebc650b695a4bb35ed7265317db
vexk1x <vexk1x.vexk1x@gmail.com>
2026-08-29 08:41:50 +0200

    Add files via upload

---
+++ Bladeball_Macro/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Bladeball_Macro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label5 = new Label();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(103, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Clicks Per Second:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(121, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Block Key 1:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 86);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Block Key 2:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 129);
+            label4.Name = "label4";
+            label4.Size = new Size(104, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Start/Stop Hotkey:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(121, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 23);
+            button1.TabIndex = 5;
+            button1.Text = "None";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(121, 82);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 23);
+            button2.TabIndex = 6;
+            button2.Text = "None";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(121, 121);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 23);
+            button3.TabIndex = 7;
+            button3.Text = "F";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 166);
+            label5.Name = "label5";
+            label5.Size = new Size(227, 15);
+            label5.TabIndex = 8;
+            label5.Text = "(Click the buttons to change the Hotkeys)";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(252, 55);
+            button4.Name = "button4";
+            button4.Size = new Size(47, 23);
+            button4.TabIndex = 9;
+            button4.Text = "Reset";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(252, 82);
+            button5.Name = "button5";
+            button5.Size = new Size(47, 23);
+            button5.TabIndex = 10;
+            button5.Text = "Reset";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(252, 9);
+            button6.Name = "button6";
+            button6.Size = new Size(47, 23);
+            button6.TabIndex = 11;
+            button6.Text = "Apply";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 37);
+            label6.Name = "label6";
+            label6.Size = new Size(177, 15);
+            label6.TabIndex = 12;
+            label6.Text = "(Automatically presses left click)";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(411, 201);
+            Controls.Add(label6);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Bladeball Macro";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label5;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Label label6;
+    }
+}
